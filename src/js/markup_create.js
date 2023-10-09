5b723acc7ab009d28624db5bca31e6734787b41b
Fa11enSky <133168352+Fa11enSky@users.noreply.github.com>
2023-10-09 17:03:21 +0300

    add styles and ligthbox

--- src/js/markup_create.js
+++ src/js/markup_create.js
@@ -3,23 +3,25 @@ function createMarkup(array) {
   return array
     .map(
       el =>
-        `<div class="photo-card">
-  <div class="thumb"><img src="${el.webformatURL}" alt="el.tags" loading="lazy"/></div>
-  <div class="info">
-    <p class="info-item">
-      <b>Likes ${el.likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views ${el.views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments ${el.comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads ${el.downloads}</b>
-    </p>
-  </div>
-</div>
+        `<a href="${el.largeImageURL}">
+            <div class="photo-card">
+      <div class="thumb"><img src="${el.webformatURL}" alt="el.tags" loading="lazy"/></div>
+      <div class="info">
+        <p class="info-item">
+          <b>Likes: ${el.likes}</b>
+        </p>
+        <p class="info-item">
+          <b>Views: ${el.views}</b>
+        </p>
+        <p class="info-item">
+          <b>Comments: ${el.comments}</b>
+        </p>
+        <p class="info-item">
+          <b>Downloads: ${el.downloads}</b>
+        </p>
+      </div>
+    </div>
+        </a>
 `
     )
     .join('');
